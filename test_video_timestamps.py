69d148dd8cccaf49d3c355dd68c52eccfa0d6c37
Ying Huang <huangyingw@gmail.com>
2023-12-05 17:19:43 -0800

    n

--- test_video_timestamps.py
+++ test_video_timestamps.py
@@ -2,8 +2,8 @@ import re
 
 
 def extract_timestamps(filename):
-    # 匹配冒号或逗号后的 HH:MM 格式的时间戳
-    pattern = r"[:,](\d{2}:\d{2})"
+    # 匹配冒号或逗号后的 HH:MM 或 H:MM:SS 格式的时间戳
+    pattern = r"[:,](\d{1,2}:\d{2}(?::\d{2})?)"
     return re.findall(pattern, filename)
 
 
@@ -13,8 +13,8 @@ import unittest
 
 class TestVideoPlayerFunctions(unittest.TestCase):
     def test_extract_timestamps(self):
-        filename = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:13:57,:09:56"
-        expected_timestamps = ["13:57", "09:56"]
+        filename = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:13:57,:1:14:15,:09:56"
+        expected_timestamps = ["13:57", "1:14:15", "09:56"]
         self.assertEqual(extract_timestamps(filename), expected_timestamps)
 
 
