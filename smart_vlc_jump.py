c511ec7d9e6feec9e742b9b8b7524c1ecc572f13
Ying Huang <huangyingw@gmail.com>
2023-12-07 08:42:34 -0800

    n

--- smart_vlc_jump.py
+++ smart_vlc_jump.py
@@ -10,7 +10,8 @@ vlc_password = "vlc_password"  # 如果设置了密码，请填写
 
 # VLC的路径和要播放的文件路径
 vlc_path = "/Applications/VLC.app/Contents/MacOS/VLC"
-file_path = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠110.rmvb"
+# file_path = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠110.rmvb"
+file_path = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:13:57,:09:56"
 
 # 从文件名解析时间戳数组
 timestamps = [10, 20, 30]  # 示例时间戳，单位为秒
