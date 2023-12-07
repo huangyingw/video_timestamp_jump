372c3066a86e607e2a7effc448eb3cbea5ccff50
Ying Huang <huangyingw@gmail.com>
2023-12-07 11:15:31 -0800

    n

--- test_video_timestamps.py
+++ test_video_timestamps.py
@@ -1,4 +1,5 @@
 import re
+from smart_vlc_jump import *
 
 
 # 单元测试部分
@@ -7,8 +8,16 @@ import unittest
 
 class TestVideoPlayerFunctions(unittest.TestCase):
     def test_extract_timestamps(self):
-        filename = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:13:57,:1:14:15,2:25:35,:09:56"
-        expected_timestamps = ["13:57", "1:14:15", "2:25:35", "09:56"]
+        filename = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:24:30,:1:11:27,:02:35:52"
+        expected_timestamps = ["24:30", "1:11:27", "02:35:52"]
+        self.assertEqual(extract_timestamps(filename), expected_timestamps)
+
+        filename = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:24:30,1:11:27,:02:35:52"
+        expected_timestamps = ["24:30", "1:11:27", "02:35:52"]
+        self.assertEqual(extract_timestamps(filename), expected_timestamps)
+
+        filename = "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:1:11:27,:24:30,:02:35:52"
+        expected_timestamps = ["24:30", "1:11:27", "02:35:52"]
         self.assertEqual(extract_timestamps(filename), expected_timestamps)
 
 
