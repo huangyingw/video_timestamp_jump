1dc0a4570ae7884ec7cb23cb961da067dd331c65
Ying Huang <huangyingw@gmail.com>
2024-01-18 11:31:57 -0800

    n

--- smart_vlc_jump.py.sh
+++ smart_vlc_jump.py.sh
@@ -3,6 +3,9 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+#pip install moviepy
+
 python ./smart_vlc_jump.py "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/" "./dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:13:57,:09:56"
+#python ./smart_vlc_jump.py "/Users/huangyingw/mini/media/usb_backup_crypt_8T_1/cartoon/" "./dragonball/第一部/龙珠 第一部 日语配音/七龙珠146.rmvb:13:57,:09:56" > smart_vlc_jump.py.runresult 2>&1
 
 cd -
